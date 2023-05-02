60de34b43a33705d106e8bae45176b731b99c774
Jessica Mendes <jessicamend17@gmail.com>
2023-05-02 12:44:31 -0300

    implementacao teste JUnit

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -61,6 +61,18 @@ public class Usuario {
 		this.nome = nome;
 	}
 
+	public Usuario(Long id, String nome, String usuario,String senha,String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+
+	public Usuario() {
+	}
+
 	public String getUsuario() {
 		return this.usuario;
 	}
